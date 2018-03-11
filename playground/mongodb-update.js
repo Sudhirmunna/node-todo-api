7213a494496cd2387f6f366846bb30ab32cb78d1
Sudhir <sudhirmunna007@gmail.com>
2018-03-11 17:13:07 -0400

    upate existing documents

---
+++ playground/mongodb-update.js
@@ -0,0 +1,28 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const url = 'mongodb://localhost:27017/TodoApp';
+
+// Connect using MongoClient
+MongoClient.connect(url, function (err, client) {
+    if (err) {
+        return console.log(' unable to connect to mongodb server');
+    }
+    console.log('Connected to mongodb server!!');
+    const db = client.db('TodoApp');
+    db.collection('Users').findOneAndUpdate({
+        name: 'Sridhar'
+    },
+        {
+            $set: {
+                name: 'Sridhar Chipuri'
+            }
+        },
+        {
+            returnOriginal: false
+        }
+    ).then(function (result) {
+        console.log(result);
+    });
+
+    client.close();
+});
